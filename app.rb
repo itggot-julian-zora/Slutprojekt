d9347b5504c518ff2d6d79d1c7a6f784ac6e88f4
itggot-julian-zora <julian.zora@itggot.se>
2016-04-21 13:54:57 +0200

    ändrat namn på filer från - till _ för att jag ska kunna referera till dem i app.rb och lagt till alla sidor

--- app.rb
+++ app.rb
@@ -5,5 +5,36 @@ class App < Sinatra::Base
     erb :index
   end
 
+  get '/difficulty_strength.erb' do
+    erb :difficulty_strength
+  end
+
+  get '/strength_beginner.erb' do
+    erb :strength_beginner
+  end
+
+  get '/strength_intermediate.erb' do
+    erb :strength_intermediate
+  end
+
+  get '/strength_advanced.erb' do
+    erb :strength_advanced
+  end
+
+  get '/difficulty_cardio.erb' do
+    erb :difficulty_cardio
+  end
+
+  get '/cardio_beginner.erb' do
+    erb :cardio_beginner
+  end
+
+  get '/cardio_intermediate.erb' do
+    erb :cardio_intermediate
+  end
+
+  get '/cardio_advanced.erb' do
+    erb :cardio_advanced
+  end
 
 end
